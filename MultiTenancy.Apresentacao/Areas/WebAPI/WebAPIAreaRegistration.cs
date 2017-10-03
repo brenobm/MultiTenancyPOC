7c30e1ad613f13b23596112d398ea5941933961d
Breno Batista Machado <breno.machado@bh.mi>
2017-10-03 18:07:37 -0300

    Correçoes

--- MultiTenancy.Apresentacao/Areas/WebAPI/WebAPIAreaRegistration.cs
+++ MultiTenancy.Apresentacao/Areas/WebAPI/WebAPIAreaRegistration.cs
@@ -2,23 +2,23 @@
 
 namespace MultiTenancy.Apresentacao.Areas.WebAPI
 {
-    public class WebAPIAreaRegistration : AreaRegistration 
-    {
-        public override string AreaName 
-        {
-            get 
-            {
-                return "WebAPI";
-            }
-        }
+    //public class WebAPIAreaRegistration : AreaRegistration 
+    //{
+    //    public override string AreaName 
+    //    {
+    //        get 
+    //        {
+    //            return "WebAPI";
+    //        }
+    //    }
 
-        public override void RegisterArea(AreaRegistrationContext context) 
-        {
-            context.MapRoute(
-                "WebAPI_default",
-                "WebAPI/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
-            );
-        }
-    }
+    //    public override void RegisterArea(AreaRegistrationContext context) 
+    //    {
+    //        context.MapRoute(
+    //            "WebAPI_default",
+    //            "WebAPI/{controller}/{action}/{id}",
+    //            new { action = "Index", id = UrlParameter.Optional }
+    //        );
+    //    }
+    //}
 }
--- MultiTenancy.Apresentacao/Global.asax.cs
+++ MultiTenancy.Apresentacao/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using MultiTenancy.Infraestrutura.Helpers;
+using System;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -32,7 +33,25 @@ namespace MultiTenancy.Apresentacao
 
         protected void Application_PreRequestHandlerExecute()
         {
-            MultiTenancyHelper.CarregarConfiguracaoCliente();
+            if (!this.Request.Url.ToString().Contains("Acesso/AcessoNegado"))
+            {
+                try
+                {
+                    MultiTenancyHelper.CarregarConfiguracaoCliente();
+                }
+                catch (Exception ex)
+                {
+
+                    this.Response.RedirectToRoute("Default",
+                        new RouteValueDictionary
+                        {
+                            { "area", "" },
+                            { "controller", "Acesso" },
+                            { "action", "AcessoNegado" },
+                            { "mensagem", ex.Message }
+                        });
+                }
+            }
         }
 
         protected void Application_PostAuthorizeRequest()
